52422cd4c1e13df7666b51ed28b6fb93e9f48c21
ic3fl4me <122876326+ic3fl4me@users.noreply.github.com>
2025-01-19 21:41:00 +0100

    Imroved movement logic and added new mechanics
    Enhanced movement (player flip, double jump), added gun + gun logic and bullets + unfinished bullet logic, improved physics + friction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,34 +1,76 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float movementSpeed = 3;
+    [SerializeField] private float movementSpeed = 7;
+    private SpriteRenderer sprite;
     private Rigidbody2D body;
+    private bool doubleJumpAvailable = true;
+    private bool isGrounded = false;
+    private Vector2 worldPosition;
+    private Vector2 direction;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
-            body.linearVelocity = new Vector3(-movementSpeed, body.linearVelocity.y, 0);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            body.linearVelocity = new Vector3(movementSpeed, body.linearVelocity.y, 0);
-        }
+        HandlePlayerRotation();
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        // Movement
+        if (horizontalInput != 0)
+            body.linearVelocity = new Vector2(horizontalInput * movementSpeed, body.linearVelocity.y);
         else
-        {
-            body.linearVelocity = new Vector3(0, body.linearVelocity.y, 0);
-        }
+            body.linearVelocity = new Vector2(0, body.linearVelocity.y);
         
-        if (Input.GetKey(KeyCode.Space)){
-            body.linearVelocity = new Vector3(body.linearVelocity.x, movementSpeed, 0);
+        // Jump
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || doubleJumpAvailable)){
+            body.linearVelocity = new Vector2(body.linearVelocity.x, movementSpeed*0.75f);
+            if (!isGrounded)
+                doubleJumpAvailable = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            doubleJumpAvailable = true;
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void HandlePlayerRotation()
+    {
+        worldPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        direction = (worldPosition - (Vector2)body.transform.position).normalized;
+
+        if (direction.x > 0)
+        {
+            // transform.localScale = new Vector3(2, 2, 1);
+            sprite.flipX = false;
+        }
+        else if (direction.x < 0) 
+        {
+            // transform.localScale = new Vector3(-2, 2, 1);
+            sprite.flipX = true;
         }
     }
 }
