442be99d3540e4c82dd893439521210c6b8b70c6
Ziireael <ralphschwarz50@gmail.com>
2025-01-27 01:04:34 +0100

    Background
    Hintergrund gemacht, die Bäume im Vordergrund geben wir eventuell weg wenn sie zu sehr stören

---
+++ Assets/Background/BackgroundController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class BackgroundController : MonoBehaviour
+{
+   
+    
+    private float startPos, length;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void FixedUpdate()
+    {
+        float distance = cam.transform.position.x * parallaxEffect;
+        float movement = cam.transform.position.x * (1 - parallaxEffect);
+
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if (movement > startPos + length)
+        {
+            startPos += length;
+        }
+        else if (movement < startPos - length)
+        {
+            startPos -= length;
+        }
+    }    
+
+}   
+    
+
